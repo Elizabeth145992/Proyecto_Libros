5cbc5ec56f2d80f0522d5a1f2c47a7fecda54361
Elizabeth145992 <37152706+Elizabeth145992@users.noreply.github.com>
2019-12-05 09:14:08 -0600

    Levantar BD4

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const mongoose= require('mongoose'); // COnstante para requerir mongoose
 const _local= 'mongodb://localhost/DBLibros';
 const _remoto= 'mongodb://ecr:1234@3.83.216.12/DBLibros?authSource=admin';
-
+ 
 // <---- Conexion y configuracion basica de moongoose ----->S
 mongoose.connect(_remoto,{
     useCreateIndex:true,
