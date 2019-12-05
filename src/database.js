c292c3fc09d4d9a7a5247a4b63ab1a04073162a9
Cristian <c.almanzaarmenta@ugto.mx>
2019-12-05 09:03:01 -0600

    db remota

--- src/database.js
+++ src/database.js
@@ -1,11 +1,9 @@
 // <-------------- CONEXION A LA BASE DE DATOS ------------>
 const mongoose= require('mongoose'); // COnstante para requerir mongoose
 const _local= 'mongodb://localhost/DBLibros';
-<<<<<<< HEAD
-const _remoto= 'mongodb://CAA:1234@3.83.216.12/DBLibros?authSource=admin';
-=======
-const _remoto= 'mongodb://NCN:1234@3.83.216.12/DBLibros?authSource=admin';
->>>>>>> develope
+
+const _remoto= 'mongodb://CAA:1234@52.90.63.108/DBLibros?authSource=admin';
+
 
 // <---- Conexion y configuracion basica de moongoose ----->S
 mongoose.connect('mongodb://localhost/DBLibros',{
